17749f8db2375209fce1754a3874514cc0e88579
SobiX <sobhanee@gmail.com>
2021-08-10 14:53:53 +0430

    h1 tag added.

--- pages/views.py
+++ pages/views.py
@@ -7,4 +7,4 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def homePageView(request):
-    return HttpResponse('Hello Sobix!')
+    return HttpResponse('<h1>Hello Sobix!</h1>')
